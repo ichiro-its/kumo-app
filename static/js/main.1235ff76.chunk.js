5ccdf124d8ace934458339570144787253bd4676
threeal <alfi.maulana.f@gmail.com>
2021-03-20 13:56:24 +0000

    Deploying to gh-pages from @ ichiro-its/kumo-app@056b4945eedb6b5139ecb669ce9ac422247661cf 🚀

---
+++ static/js/main.1235ff76.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpkumo-app"]=this["webpackJsonpkumo-app"]||[]).push([[0],{9:function(t,e,c){"use strict";c.r(e);var n=c(0),r=c.n(n),o=c(3),s=c(1);var p=function(){return Object(s.jsx)("div",{})};Object(o.render)(Object(s.jsx)(r.a.StrictMode,{children:Object(s.jsx)(p,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.1235ff76.chunk.js.map
