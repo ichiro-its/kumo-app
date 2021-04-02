06b6067a454113ae3859489a87cf5e3efce13886
Bryan Yehuda <bryanyehuda@gmail.com>
2021-04-02 20:16:21 +0700

    Fixing Serviceworker.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,15 @@
 if (!("serviceWorker" in navigator)) {
+  console.error("ServiceWorker: Browser tidak mendukung.");
 } else {
   navigator.serviceWorker
     .register("../service-worker.js")
     .then(function(registration) {
+      console.log(
+        "ServiceWorker: Pendaftaran berhasil. Scope:",
+        registration.scope
+      );
     })
     .catch(function(error) {
+      console.error("ServiceWorker: Pendaftaran gagal. Error:", error);
     });
-}
+} 
